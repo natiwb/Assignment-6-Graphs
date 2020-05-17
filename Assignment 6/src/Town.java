0f971ffa3c7d53e5926c1d3aa48cd25d3ed7c40d
natiwb <nathanbekele120@gmail.com>
2020-05-17 09:25:27 -0400

    Begin with town class as well

---
+++ Assignment 6/src/Town.java
@@ -0,0 +1,27 @@
+
+public class Town
+{
+    String name;
+    
+	public Town(String name) 
+	{
+		this.name=name;
+	}
+	
+	public Town(Town debreMarkos) 
+	{
+		this.name=debreMarkos.name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int get
+
+	
+}
